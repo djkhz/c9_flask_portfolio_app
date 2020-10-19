906967bd4277a87abb3226f5201dff64118c1218
djkhz <djkhz@yahoo.com>
2020-10-19 17:17:40 +0700

    update

--- BK/README.md
+++ BK/README.md
@@ -1,7 +1,4 @@
-# Flask Portfolio App for The Profitable Programmer Students...
+# c9_flask_portfolio_app
 Flask Portfolio App for students @ Clever Programmer deployed from Cloud 9
 
 [![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
-
-## LIVE PORTFOLIO APP...
-https://my-portfolio-with-flask.herokuapp.com/
---
+++ BK/app.py
@@ -0,0 +1,156 @@
+from flask import Flask, render_template, request, redirect
+import datetime
+import pytz # timezone 
+import requests
+import os
+from flask import Flask, jsonify
+from flask import Flask, make_response
+from flask import Flask, redirect, url_for
+import flask_sijax
+#https://gist.github.com/spantaleev/4433109
+#from flask_sijax import sijax
+
+
+app = Flask(__name__)
+
+app.config["SIJAX_STATIC_PATH"] = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
+app.config["SIJAX_JSON_URI"] = '/static/js/sijax/json2.js'
+flask_sijax.Sijax(app)
+
+
+@app.route('/', methods=['GET'])
+def home_page():
+	return render_template('index.html')
+
+@app.route('/<name>')
+def profile(name):
+	return render_template('index.html', name=name)
+
+
+@app.route('/add_numbers', methods=['GET','POST'])
+def add_numbers_post():
+	  # --> ['5', '6', '8']
+	  # print(type(request.form['text']))
+	  if request.method == 'GET':
+	  	return render_template('add_numbers.html')
+	  elif request.method == 'POST':
+  	      print(request.form['text'].split())
+  	      total = 0
+  	      try:
+  	      	for str_num in request.form['text'].split():
+  	      		total += int(str_num)
+  	      	return render_template('add_numbers.html', result=str(total))
+  	      except ValueError:
+  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
+
+
+@app.route('/shopping_list', methods=['GET','POST'])
+def shopping_list_post():
+	  # --> ['5', '6', '8']
+	  # print(type(request.form['text']))
+
+    if request.method == 'GET':
+      return render_template('shopping_list.html')
+    elif request.method == 'POST':
+          print(request.form['text'].split())
+          
+          shop_list = []
+          try:
+            for item in request.form['text'].split():
+              
+              shop_list.append(item)
+
+              
+              
+            return render_template('shopping_list.html', result="\n".join([str(item) for item in shop_list]))
+          except ValueError:
+            return "Easy now! Let's keep it simple! Just words with a space between them"
+          
+  	      
+@app.route('/time', methods=['GET','POST'])
+def time_post():
+    # --> ['5', '6', '8']
+    # print(type(request.form['text']))
+
+    if request.method == 'GET':
+      return render_template('time.html')
+    elif request.method == 'POST':
+          print(request.form['text'].split())
+          
+          for item in request.form['text'].split():
+            answer = (datetime.datetime.now(pytz.timezone("Europe/Dublin")).strftime('Time = ' + '%H:%M:%S' + ' GMT ' + ' Year = ' + '%d-%m-%Y'))
+            #answer = datetime.datetime.now().strftime('Time == ' + '%H:%M:%S' + ' Year == ' + '%d-%m-%Y')
+            #answer = datetime.datetime.now().strftime('%Y-%m-%d \n %H:%M:%S')
+
+              
+              
+            return render_template('time.html', result=answer)
+
+         
+
+@app.route('/python_apps')
+def python_apps_page():
+	# testing stuff
+	return render_template('python_apps.html')
+
+@app.route('/test')
+def test_page():
+    """Return homepage."""
+    json_data = {'Hello': 'World der!'}
+    return jsonify(json_data)
+
+def generateMetrics():
+    return "hello world"
+
+
+@app.route('/metrics')
+def metrics():
+    response = make_response(generateMetrics(), 200)
+    response.mimetype = "text/plain"
+    return response
+
+@app.route('/metrics2')
+def metrics2():
+    return generateMetrics()
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
+@app.route('/blog', methods=['GET'])
+def blog_page():
+  return render_template('blog.html')
+
+@app.route("/jax")
+def hello():
+    return "Hello World!<br /><a href='/sijax'>Go to Sijax test</a>"
+
+# Sijax enabled function - notice the `@Sijax.route` decorator
+# used instead of `@app.route` (above).
+@flask_sijax.route(app, "/sijax")
+def hello_sijax():
+    # Sijax handler function receiving 2 arguments from the browser
+    # The first argument (obj_response) is passed automatically
+    # by Sijax (much like Python passes `self` to object methods)
+    def hello_handler(obj_response, hello_from, hello_to):
+        obj_response.alert('Hello from %s to %s' % (hello_from, hello_to))
+        obj_response.css('a', 'color', 'green')
+
+    # Another Sijax handler function which receives no arguments
+    def goodbye_handler(obj_response):
+        obj_response.alert('Goodbye, whoever you are.')
+        obj_response.css('a', 'color', 'red')
+
+    if g.sijax.is_sijax_request:
+        # The request looks like a valid Sijax request
+        # Let's register the handlers and tell Sijax to process it
+        g.sijax.register_callback('say_hello', hello_handler)
+        g.sijax.register_callback('say_goodbye', goodbye_handler)
+        return g.sijax.process_request()
+
+    return render_template('hello.html')
+
+app.run(host=os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
+
+if __name__ == '__main__':
+	app.run(debug=False)
